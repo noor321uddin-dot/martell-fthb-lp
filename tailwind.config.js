b004e4a293d4f372edbb9014821c5713da76a51b
Noor <noor@getlevrg.com>
2026-07-30 05:39:33 +0600

    chore: scaffold Tailwind build + page skeletons

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./public/**/*.html", "./src/js/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        martell: {
+          teal: "#168876"
+        }
+      },
+      fontFamily: {
+        serif: ["Caudex", "Georgia", "serif"]
+      }
+    }
+  },
+  plugins: []
+};
